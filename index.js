3fd8bb1b33a8dcbbbc766a226857092807ac9b52
Andrew Ruiz <nukedoom@gmail.com>
2020-12-08 23:16:23 -0800

    Uploading

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code away!
+
+const server = require('./server')
+const port = 6000
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`)
+})
--- server.js
+++ server.js
@@ -2,12 +2,24 @@ const express = require('express');
 
 const server = express();
 
+// Importing our routes
+const userRouter = require("./users/userRouter")
+const postRouter = require("./posts/postRouter")
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+// Making sure our server communicates in JSON
+server.use(express.json())
+
+
+server.use(logger)
+
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(req.url, res.method)
+}
 
 module.exports = server;
